326ad335d4b97db276aa01577c3937a4017042c5
iamudyavar <aditya.udyavar@gmail.com>
2025-10-20 11:41:17 -0500

    Add analytics using PostHog

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { NetworkBanner } from '../components/NetworkBanner';
 import { Colors } from '../theme/colors';
 import { useFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold } from '@expo-google-fonts/inter';
 import { ActivityIndicator, View, StatusBar } from 'react-native';
+import { PostHogProvider } from 'posthog-react-native';
 
 export default function RootLayout() {
     // Load fonts for the application
@@ -26,29 +27,38 @@ export default function RootLayout() {
 
     return (
         <View style={{ flex: 1, backgroundColor: Colors.background }}>
-            <NetworkProvider>
-                <ConvexClientProvider>
-                    <StatusBar barStyle="light-content" backgroundColor={Colors.background} />
-                    <Stack screenOptions={{
-                        headerStyle: {
-                            backgroundColor: Colors.surface,
-                        },
-                        headerTintColor: '#fff',
-                        headerTitleStyle: {
-                            fontFamily: 'Inter_600SemiBold',
-                        },
-                        contentStyle: {
-                            backgroundColor: Colors.background
-                        },
-                        gestureEnabled: true,
-                        gestureDirection: 'horizontal',
-                    }}>
-                        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                        <Stack.Screen name="match/[vlrId]" options={{ title: 'Match Details', headerBackTitle: 'Back' }} />
-                    </Stack>
-                    <NetworkBanner />
-                </ConvexClientProvider>
-            </NetworkProvider>
+            <PostHogProvider
+                apiKey={process.env.EXPO_PUBLIC_POSTHOG_API_KEY!}
+                options={{
+                    host: 'https://us.i.posthog.com',
+                    enableSessionReplay: true,
+                }}
+                autocapture
+            >
+                <NetworkProvider>
+                    <ConvexClientProvider>
+                        <StatusBar barStyle="light-content" backgroundColor={Colors.background} />
+                        <Stack screenOptions={{
+                            headerStyle: {
+                                backgroundColor: Colors.surface,
+                            },
+                            headerTintColor: '#fff',
+                            headerTitleStyle: {
+                                fontFamily: 'Inter_600SemiBold',
+                            },
+                            contentStyle: {
+                                backgroundColor: Colors.background
+                            },
+                            gestureEnabled: true,
+                            gestureDirection: 'horizontal',
+                        }}>
+                            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                            <Stack.Screen name="match/[vlrId]" options={{ title: 'Match Details', headerBackTitle: 'Back' }} />
+                        </Stack>
+                        <NetworkBanner />
+                    </ConvexClientProvider>
+                </NetworkProvider>
+            </PostHogProvider>
         </View>
     );
 }
